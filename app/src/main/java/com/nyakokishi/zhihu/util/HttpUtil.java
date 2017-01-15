817ee34f3775f4b6a95827d9559d7ece4c0f8992
nyakokishi <860321158@qq.com>
2017-01-15 18:17:21 +0800

    modify HttpUtil

--- app/src/main/java/com/nyakokishi/zhihu/util/HttpUtil.java
+++ app/src/main/java/com/nyakokishi/zhihu/util/HttpUtil.java
@@ -4,48 +4,11 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-import com.loopj.android.http.TextHttpResponseHandler;
 
 /**
  * Created by nyakokishi on 2016/3/22.
  */
 public class HttpUtil {
-    private static AsyncHttpClient httpClient = new AsyncHttpClient();
-
-    public static void get(String url, AsyncHttpResponseHandler responseHandler) {
-        httpClient.get(url, responseHandler);
-    }
-
-    public static void get(String url, TextHttpResponseHandler responseHandler) {
-        httpClient.get(url, responseHandler);
-    }
-
-    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        httpClient.get(url, params, responseHandler);
-    }
-
-    public static void get(String url, RequestParams params, TextHttpResponseHandler responseHandler) {
-        httpClient.get(url, params, responseHandler);
-    }
-
-    public static void post(String url, AsyncHttpResponseHandler responseHandler) {
-        httpClient.post(url, responseHandler);
-    }
-
-    public static void post(String url, TextHttpResponseHandler responseHandler) {
-        httpClient.post(url, responseHandler);
-    }
-
-    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        httpClient.post(url, params, responseHandler);
-    }
-
-    public static void post(String url, RequestParams params, TextHttpResponseHandler responseHandler) {
-        httpClient.post(url, params, responseHandler);
-    }
 
     /**
      * 检测当的网络（WLAN、3G/2G）状态
